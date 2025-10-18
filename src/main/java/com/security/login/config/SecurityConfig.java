e1adb97063374b530a9de22a33e369c7ec42f821
LeeTaeWon <ltw0781@naver.com>
2025-10-19 01:01:32 +0900

    first commit

---
+++ src/main/java/com/security/login/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.security.login.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)                         // 로그인 페이지 비활성화
+            .httpBasic(Customizer.withDefaults())                               // HTTP Basic 인증 커스텀 설정
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/public/**").permitAll()
+                .requestMatchers("/private/**").authenticated()
+                .anyRequest().denyAll());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/com/security/login/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.security.login.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+    
+    @GetMapping("/public/hello")
+    public String publicHello() {
+        return"public Hello World";
+    }
+    
+    @GetMapping("/private/hello")
+    public String privateHello() {
+        return"private Hello World";
+    }
+
+}
